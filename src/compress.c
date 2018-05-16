451da8d22321c3395ef1b3a57ba83037d836c7f9
kdex <kdex@kdex.de>
2018-05-16 22:07:39 +0200

    Removed window-sliding code
    See README for more information why this was done.

--- src/compress.c
+++ src/compress.c
@@ -6,38 +6,29 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-/* TODO: remove */
-#include <stdio.h>
 size_t compress(const u8 *restrict uncompressed, size_t uncompressedSize, u8 **restrict compressed) {
 	CompressionContext *context = malloc(sizeof *context);
 	initCompressionContext(context);
-	const size_t maxWindowSize = context->large->capacity;
 	size_t compressedSize = 0;
 	const size_t maxIndex = uncompressedSize - 1;
-	/* Index of where the window should start. Indicates first position of yet uncompressed data */
-	size_t searchStart = 0;
-	/* Index of where the window should end. Indicates last position of yet uncompressed data */
-	size_t windowEnd = min(maxWindowSize - 1, maxIndex);
-	/* TODO: Implement */
-	size_t lookAheadStart = 0;
+	size_t position = 0;
 	size_t bufferSize = 0;
 	size_t bufferOffset = 0;
-	while (lookAheadStart < uncompressedSize) {
+	while (position < uncompressedSize) {
 		bool lzFailed = false;
-		const size_t windowSize = windowEnd - searchStart + 1;
-		if (searchStart < lookAheadStart) {
+		if (position) {
 			/* Dictionary exists */
-			const size_t availableLookAhead = windowEnd - lookAheadStart + 1;
-			size_t i = lookAheadStart - 1;
+			const size_t availableLookAhead = maxIndex - position + 1;
+			size_t i = position - 1;
 			size_t bestOffset;
 			u16 matchLength = 0;
 			/* TODO: Reverse this search to return early */
 			do {
-				const bool isMatch = uncompressed[i] == uncompressed[lookAheadStart];
+				const bool isMatch = uncompressed[i] == uncompressed[position];
 				if (isMatch) {
 					u16 length = 1;
-					for (size_t j = 1; j < min(availableLookAhead, lookAheadStart - i); ++j) {
-						if (uncompressed[i + j] == uncompressed[lookAheadStart + j]) {
+					for (size_t j = 1; j < min(availableLookAhead, position - i); ++j) {
+						if (uncompressed[i + j] == uncompressed[position + j]) {
 							++length;
 						}
 					}
@@ -47,7 +38,7 @@ size_t compress(const u8 *restrict uncompressed, size_t uncompressedSize, u8 **r
 					}
 				}
 			}
-			while (i-- != searchStart);
+			while (i--);
 			if (matchLength) {
 				if (bufferSize) {
 					compressedSize += compressUncompressed(context, bufferSize, uncompressed + bufferOffset);
@@ -55,66 +46,60 @@ size_t compress(const u8 *restrict uncompressed, size_t uncompressedSize, u8 **r
 				}
 				/* Matches found */
 				compressedSize += compressCopy(context, matchLength, COPY_BYTES, bestOffset);
-				lookAheadStart += matchLength;
-// 				searchStart = min(searchStart + matchLength, maxIndex);
-				windowEnd = min(windowEnd + matchLength, maxIndex);
+				position += matchLength;
 			}
 			else {
 				lzFailed = true;
 			}
-			/* TODO: Write out uncompressed buffer if possible? */
 		}
 		else {
 			lzFailed = true;
 		}
 		if (lzFailed) {
-			const bool checkPairs = searchStart + 2 <= maxIndex;
-			const u8 byteA = uncompressed[searchStart];
+			const bool checkPairs = position + 3 <= maxIndex;
+			const u8 byteA = uncompressed[position];
 			u8 byteB;
-			u16 pairedBytes = 1;
 			u16 matched = 1;
 			CompressionMode leader = UNCOMPRESSED;
 			{
 				if (checkPairs) {
-					byteB = uncompressed[searchStart + 1];
-					for (size_t i = searchStart + 2; i < searchStart + windowSize - 1; i += 2) {
+					u16 matches = 1;
+					byteB = uncompressed[position + 1];
+					for (size_t i = position + 2; i < uncompressedSize - 1; i += 2) {
 						if (uncompressed[i] != byteA || uncompressed[i + 1] != byteB) {
 							break;
 						}
-						++pairedBytes;
+						++matches;
 					}
-					if (matched < pairedBytes) {
-						matched = 2 * pairedBytes;
+					if (matched < matches) {
+						matched = 2 * matches;
 						leader = FILL_BYTES;
 					}
 				}
 			}
 			{
-				/* Check for FILL_BYTE */
-				u16 constantBytes = 0;
-				for (size_t i = searchStart; i < searchStart + windowSize; ++i) {
+				u16 matches = 0;
+				for (size_t i = position; i < uncompressedSize; ++i) {
 					if (uncompressed[i] != byteA) {
 						break;
 					}
-					++constantBytes;
+					++matches;
 				}
-				if (matched < constantBytes) {
-					matched = constantBytes;
+				if (matched < matches) {
+					matched = matches;
 					leader = FILL_BYTE;
 				}
 			}
 			{
-				u16 incrementalBytes = 0;
-				/* Check for FILL_INCREMENTAL_SEQUENCE */
-				for (size_t i = 0; i < searchStart + windowSize; ++i) {
-					/* TODO: Cast necessary? */
-					if (uncompressed[i] != (u8) (byteA + i)) {
+				u16 matches = 0;
+				for (size_t i = 0; i < uncompressedSize - position; ++i) {
+					if (uncompressed[position + i] != (u8) (byteA + i)) {
 						break;
 					}
-					++incrementalBytes;
+					++matches;
 				}
-				if (matched < incrementalBytes) {
-					matched = incrementalBytes;
+				if (matched < matches) {
+					matched = matches;
 					leader = FILL_INCREMENTAL_SEQUENCE;
 				}
 			}
@@ -134,14 +119,12 @@ size_t compress(const u8 *restrict uncompressed, size_t uncompressedSize, u8 **r
 					break;
 				default:
 					if (!bufferSize) {
-						bufferOffset = lookAheadStart;
+						bufferOffset = position;
 					}
 					++bufferSize;
 					break;
 			}
-// 			searchStart += min(searchStart + constantBytes, maxIndex);
-// 			windowEnd = min(windowEnd + constantBytes, maxIndex);
-			lookAheadStart += matched;
+			position += matched;
 		}
 	}
 	if (bufferSize) {
---
+++ test/compression/mixed.c
@@ -0,0 +1,18 @@
+#include "test-tools.h"
+#include "types.h"
+#include <stdlib.h>
+int main(void) {
+	const u8 uncompressed[] = {
+		1, 2, 3, 4, 5,
+		10, 10, 10, 10, 10,
+		8, 9, 8, 9, 8, 9,
+		8, 9, 8, 9, 8, 9
+	};
+	const u8 compressed[] = {
+		100, 1,
+		36, 10,
+		69, 8, 9,
+		END
+	};
+	return test(compress, uncompressed, sizeof uncompressed, compressed, sizeof compressed);
+}
